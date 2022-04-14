b71a2fbb02581410b4fa04f66ea3294c4b2861ba
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-14 15:16:20 +1200

    Create budget modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Button, Stack } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import BudgetCard from './components/BudgetCard';
+import AddBudgetModal from './contexts/AddBudgetModal';
 
 function App() {
   return (
+    <>
     <Container className="my-4">
       <Stack direction="horizontal" gap="2" className="mb-4">
         <h1 className="me-auto">Budgets</h1>
@@ -14,6 +16,8 @@ function App() {
         <BudgetCard name="Entertainment" gray amount={200} max={1000}></BudgetCard>
       </div>
     </Container>
+    <AddBudgetModal show />
+    </>
   );
 }
 
